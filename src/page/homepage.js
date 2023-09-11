82250896687aa41723a9df4f38ad6f98784bc5af
saurabhp789 <saurabh.prajapati@secret-spirit.com>
2023-09-11 19:03:31 +0530

    worked on header (wip)

--- src/page/homepage.js
+++ src/page/homepage.js
@@ -3,29 +3,34 @@ import * as React from 'react';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import Header from '../components/header'
+import Box from "@mui/material/Box";
+
 
 
 export default function Homepage() {
   return (
-    <div>
-      <Breadcrumbs maxItems={2} aria-label="breadcrumb">
-        <Link underline="hover" color="inherit" href="#">
-          Home
-        </Link>
-        <Link underline="hover" color="inherit" href="#">
-          Catalog
-        </Link>
-        <Link underline="hover" color="inherit" href="#">
-          Accessories
-        </Link>
-        <Link underline="hover" color="inherit" href="#">
-          New Collection
-        </Link>
-        <Typography color="text.primary">Belts</Typography>
-      </Breadcrumbs>
-    </div>
+    <>
+      <Header />
+      <Box>
+        <Breadcrumbs maxItems={2} aria-label="breadcrumb">
+          <Link underline="hover" color="inherit" href="#">
+            Home
+          </Link>
+          <Link underline="hover" color="inherit" href="#">
+            Catalog
+          </Link>
+          <Link underline="hover" color="inherit" href="#">
+            Accessories
+          </Link>
+          <Link underline="hover" color="inherit" href="#">
+            New Collection
+          </Link>
+          <Typography color="text.primary">Belts</Typography>
+        </Breadcrumbs>
+      </Box>
+
+    </>
   );
 }
 
