f990f608b231248d4cfd8d57444cc99b15d4976b
uttamkhanpara <uttam.khanpara@secret-spirit.com>
2023-09-11 12:39:27 +0530

    working on poc task

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import '../assets/scss/style.scss'
+import Homepage from '../page/homepage'
+
+export default function Home() {
+  return (
+   <div className='test'>
+   <Homepage />
+    </div>
+  )
+}
---
+++ src/page/homepage.js
@@ -0,0 +1,31 @@
+// import React from "react";
+import * as React from 'react';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+
+export default function Homepage() {
+  return (
+    <div>
+      <Breadcrumbs maxItems={2} aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="#">
+          Home
+        </Link>
+        <Link underline="hover" color="inherit" href="#">
+          Catalog
+        </Link>
+        <Link underline="hover" color="inherit" href="#">
+          Accessories
+        </Link>
+        <Link underline="hover" color="inherit" href="#">
+          New Collection
+        </Link>
+        <Typography color="text.primary">Belts</Typography>
+      </Breadcrumbs>
+    </div>
+  );
+}
+
